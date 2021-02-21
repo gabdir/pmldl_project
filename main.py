b36d3bd55ea2a157e34d387d80623ca76c2e1fd6
Maxim Popov <maximpop1@yandex.ru>
2021-02-21 18:10:46 +0300

    small fix

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from tinder.tinder_controller import TinderController
 
 if __name__ == '__main__':
     controller = TinderController()
-    # controller.start_dataset_collecting('dataset')
+    controller.start_dataset_collecting('dataset')
