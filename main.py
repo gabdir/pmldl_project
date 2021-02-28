2a3021b90d2cc9aff31c77a9b23c95da5d7a417c
gabdir <a.gabdrahmanov@innopolis.ru>
2021-02-28 20:23:33 +0300

    report

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import argparse
 
 from tinder.tinder_controller import TinderController
+from tinder.conversations.app import app
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', dest='d', action='store_true', help='to run in dataset collection mode')
-    parser.add_argument('-f', '--folder', dest='f', default='dataset', help='where to store data in dataset collection mode')
-    args = parser.parse_args()
-
-    controller = TinderController()
-    if args.d:
-        controller.start_dataset_collecting(args.f)
-    else:
-        # auto-mode
-        pass
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-d', dest='d', action='store_true', help='to run in dataset collection mode')
+    # parser.add_argument('-f', '--folder', dest='f', default='dataset', help='where to store data in dataset collection mode')
+    # args = parser.parse_args()
+    #
+    # controller = TinderController()
+    # if args.d:
+    #     controller.start_dataset_collecting(args.f)
+    # else:
+    #     # auto-mode
+    #     pass
+    app.run()
